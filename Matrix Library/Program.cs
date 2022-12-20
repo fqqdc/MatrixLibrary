350a028c909c45755f5f026f0c1892d8149b27e4
Emanon <fqqdc@qq.com>
2022-12-20 14:18:05 +0800

    添加求向量长度

--- Matrix Library/Program.cs
+++ Matrix Library/Program.cs
@@ -8,25 +8,14 @@ namespace MatrixLibrary
     {
         static void Main(string[] args)
         {
-            Matrix<int> m1 = new(new(1), new(2), new(3));
-            Console.WriteLine(m1);
-            
-            Matrix<int> m2 = new(new Vector<int>(1, 2, 3));
-            Console.WriteLine(m2);
+            var v2 = new Vector<double>(1, 1);
+            Console.WriteLine(v2.GetLength());
 
-            Console.WriteLine(m1 * m2);
-            Console.WriteLine(m2 * m1);
-            //var m = (m2 * m1).TransposeMatrix() + new Vector<int>(10, 20, 30);
-            var m = (m2 * m1).TransposeMatrix() * 100;
-            Console.WriteLine(m.TransposeMatrix());
+            var v3 = new Vector<double>(1, 1, 1);
+            Console.WriteLine(v3.GetLength());
 
-            Matrix<double> m3 = new(new(1.2, 2.3), new(3.4, 4.5));
-            Console.WriteLine(m3);
-            Console.WriteLine(m3 * m3);
-            Console.WriteLine(m3 + m3);
-
-            Console.WriteLine(Matrix<double>.GetIdentity(3));
-            Console.WriteLine(Matrix<double>.GetIdentity(5));
+            var v4 = new Vector<double>(1, 0, 0, 1);
+            Console.WriteLine(v4.GetLength());
         }
     }
 }
---
+++ Matrix Library/VectorExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLibrary
+{
+    public static class VectorExtension
+    {
+        public static T GetLength<T>(this Vector<T> vector) 
+            where T : IRootFunctions<T>
+        {
+            T sum = T.Zero;
+            for (int i = 0; i < vector.Dimension; i++)
+            {
+                sum += vector.Values[i] * vector.Values[i];
+            }
+            return T.Sqrt(sum);
+        }
+    }
+}
