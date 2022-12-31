b52645aa546692a5e8c9ded69440f611610df3dc
Emanon <fqqdc@qq.com>
2022-12-31 20:55:32 +0800

    Update VectorExtension.cs

--- Matrix Library/VectorExtension.cs
+++ Matrix Library/VectorExtension.cs
@@ -11,31 +11,26 @@ namespace MatrixLibrary
     {
         public static T GetLength<T>(this Vector<T> vector) where T : IRootFunctions<T>
         {
-            T sum = T.Zero;
-            for (int i = 0; i < vector.Dimension; i++)
-            {
-                sum += vector.Values[i] * vector.Values[i];
-            }
-            return T.Sqrt(sum);
+            return T.Sqrt(vector.Dot(vector));
         }
 
-        public static Vector<T> Normalized<T>(this Vector<T> vector) where T : IRootFunctions<T>
+        public static Vector<T> Normalize<T>(this Vector<T> vector) where T : IRootFunctions<T>
         {
             return vector / vector.GetLength();
         }
 
-        public static Vector<T> Dot<T>(this Vector<T> vector1, Vector<T> vector2) where T : INumberBase<T>
+        public static T Dot<T>(this Vector<T> vector1, Vector<T> vector2) where T : INumberBase<T>
         {
             if (vector1.Dimension != vector2.Dimension)
                 throw new ArgumentException($"{nameof(vector1)}与{nameof(vector2)}的维度必须相等。");
 
-            T[] newValue = new T[vector1.Dimension];
-            for (int i = 0; i < newValue.Length; i++)
+            T sum = T.Zero;
+            for (int i = 0; i < vector1.Dimension; i++)
             {
-                newValue[i] = vector1.Values[i] * vector2.Values[i];
+                sum += vector1.Values[i] * vector2.Values[i];
             }
 
-            return new Vector<T>(newValue);
+            return sum;
         }
     }
 }
